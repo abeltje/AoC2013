f120168ae17c84725891fd99b1a3a706ea3a72ed
Abe Timmerman <abeltje@cpan.org>
2023-12-01 12:57:48 +0100

    rename my_reverse to str_reverse

--- code/day1.pl
+++ code/day1.pl
@@ -5,6 +5,7 @@ GetOptions(
     'prd'    => \my $prd,
     'part:1' => \my $part,
 );
+$part //= 1;
 say "Part: $part";
 my $day = 1;
 my $sample = "code/sample$day-$part";
@@ -29,17 +30,17 @@ else {
     my @digits = qw< one two three four five six seven eight nine >;
     my $i = 1;
     my %value = map { ($_ => $i++) } @digits;
-    $i = 1; $value{my_reverse($_)} = $i++ for @digits;
+    $i = 1; $value{str_reverse($_)} = $i++ for @digits;
     $value{$_} = $_ for 1..9;
     my $fwd = join("|", @digits);
-    my $bwd = join("|", map { my_reverse($_) } @digits);
+    my $bwd = join("|", map { str_reverse($_) } @digits);
 
     my $total = 0;
     open(my $in, '<', $input) or die "Cannot open($input): $!";
     while (my $line = <$in>) {
         chomp($line);
         my ($first) = $line =~ m{^.*?([0-9]|$fwd)};
-        my ($last)  = my_reverse($line) =~ m{^.*?([0-9]|$bwd)};
+        my ($last)  = str_reverse($line) =~ m{^.*?([0-9]|$bwd)};
         $total += 0 + "$value{$first}$value{$last}";
         say "($first, $last) $value{$first}$value{$last} => $line";
     }
@@ -48,6 +49,6 @@ else {
 }
 
 
-sub my_reverse ($string) {
+sub str_reverse ($string) {
     return scalar(reverse($string));
 }
