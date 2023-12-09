05c6029117c5603bca5c63cc0564c732bb940193
Abe Timmerman <abeltje@cpan.org>
2023-12-09 09:40:47 +0100

    Add day 6; part 2

--- code/day6.pl
+++ code/day6.pl
@@ -37,3 +37,25 @@ if ($part == 1) {
     }
     say "Total: $total";
 }
+else {
+    my $total = 1;
+    open(my $in, '<', $input) or die "Cannot open($input): $!";
+    (my $time = <$in>) =~ s{^Time:\s+}{}; $time =~ tr{ }{}d;
+    my @times = split(" ", $time);
+    (my $dist = <$in>) =~ s{^Distance:\s+}{}; $dist =~ tr{ }{}d;
+    my @dists = split(" ", $dist);
+    close($in);
+    # v = s; d = (t-s) * v => d = s*(t-s) > dm; st - s2 > dm; -s2 + ts -dm > 0
+    # (-b +- sqrt(b2 - 4ac))/2a: (-t ± sqrt(t*t - 4*-1*-dm))/-2
+    for my $i (0 .. $#times) {
+        my $t = $times[$i];
+        my $d = $dists[$i];
+        my $s1 = (-1 * $t + sqrt($t * $t + 4*-1*$d))/-2;
+        my $s2 = (-1 * $t - sqrt($t * $t + 4*-1*$d))/-2;
+        $s1 = 1 + int($s1);
+        $s2 = int($s2) == $s2 ? $s2 - 1 : int($s2);
+        say "$t ($d) $s1 - $s2 => @{[ $s2 - $s1 + 1 ]}";
+        $total *= ($s2 - $s1 + 1);
+    }
+    say "Total: $total";
+}
