6532c494544ef66c827d45db2e44c468446a931c
Abe Timmerman <abeltje@cpan.org>
2023-12-09 11:14:39 +0100

    Add day7; part 2

--- code/day7.pl
+++ code/day7.pl
@@ -15,11 +15,12 @@ my $real = "data/day$day-$part";
 
 my $input = $prd ? $real : $sample;
 
+my (@cards, %cv, @type, %tv);
 if ($part == 1) {
-    my @cards = qw< A K Q J T 9 8 7 6 5 4 3 2 >;
-    my $i = 1; my %cv = map { ($_ => $i++) } reverse(@cards);
-    my @type = qw< 5 4 F 3 T 2 H >;
-    $i = 1; my %tv = map { ($_ => $i++) } reverse(@type);
+    @cards = qw< A K Q J T 9 8 7 6 5 4 3 2 >;
+    my $i = 1; %cv = map { ($_ => $i++) } reverse(@cards);
+    @type = qw< 5 4 F 3 T 2 H >;
+    $i = 1; %tv = map { ($_ => $i++) } reverse(@type);
 
     my @games;
     open(my $in, '<', $input) or die "Cannot open($input): $!";
@@ -29,22 +30,7 @@ if ($part == 1) {
     }
     close($in);
     for my $game (@games) {
-        my %gt;
-        $gt{$_}++ for split(/|/, $game->[0]);
-        my $t = keys(%gt) == 1 ? '5'
-            : keys(%gt) == 5 ? 'H'
-            : '?';
-        if ($t eq '?') {
-            if (grep { m/^4$/ } values(%gt)) {
-                $t = '4';
-            }
-            elsif (grep { m/^3$/ } values(%gt)) {
-                $t = (grep { m/^2$/ } values(%gt)) ? 'F' : '3';
-            }
-            elsif (my $cnt = grep { m/^2$/ } values(%gt)) {
-                $t = $cnt > 1 ? 'T' : '2';
-            }
-        }
+        my $t = gametype($game->[0]);
         push(@$game, $t);
     }
     @games = sort {
@@ -62,4 +48,74 @@ if ($part == 1) {
         say "@{$games[$i]} ($rank)";
     }
     say "Total: $total";
+}
+else {
+    @cards = qw< A K Q T 9 8 7 6 5 4 3 2 J >;
+    my $i = 1; %cv = map { ($_ => $i++) } reverse(@cards);
+    @type = qw< 5 4 F 3 T 2 H >;
+    $i = 1; %tv = map { ($_ => $i++) } reverse(@type);
+
+    my @games;
+    open(my $in, '<', $input) or die "Cannot open($input): $!";
+    while (my $line = <$in>) {
+        chomp($line);
+        push(@games, [ split(" ", $line) ]);
+    }
+    close($in);
+
+    for my $game (@games) {
+        my $t = best_gametype($game->[0]);
+        push(@$game, $t);
+    }
+    @games = sort {
+           $tv{$b->[2]} <=> $tv{$a->[2]}
+        || $cv{ substr($b->[0], 0, 1) } <=> $cv{ substr($a->[0], 0, 1) }
+        || $cv{ substr($b->[0], 1, 1) } <=> $cv{ substr($a->[0], 1, 1) }
+        || $cv{ substr($b->[0], 2, 1) } <=> $cv{ substr($a->[0], 2, 1) }
+        || $cv{ substr($b->[0], 3, 1) } <=> $cv{ substr($a->[0], 3, 1) }
+        || $cv{ substr($b->[0], 4, 1) } <=> $cv{ substr($a->[0], 4, 1) }
+    } @games;
+    my $total = 0;
+    for my $i (0 .. $#games) {
+        my $rank = @games - $i;
+        $total += ($rank * $games[$i]->[1]);
+        say "@{$games[$i]} ($rank)";
+    }
+    say "Total: $total";
+}
+
+sub gametype ($hand) {
+    my %gt;
+    $gt{$_}++ for split(/|/, $hand);
+
+    my $t;
+    if (keys(%gt) == 1) {
+        $t = '5';
+    }
+    elsif (keys(%gt) == 5) {
+        $t = 'H';
+    }
+    elsif (grep { m/^4$/ } values(%gt)) {
+        $t = '4';
+    }
+    elsif (grep { m/^3$/ } values(%gt)) {
+        $t = (grep { m/^2$/ } values(%gt)) ? 'F' : '3';
+    }
+    elsif (my $cnt = grep { m/^2$/ } values(%gt)) {
+        $t = $cnt > 1 ? 'T' : '2';
+    }
+    return $t;
+}
+
+sub best_gametype ($hand) {
+    return gametype($hand) unless $hand =~ m/J/;
+    my $best = 'H'; my $card = 'A';
+    for my $subst (@cards[0..$#cards-1]) {
+        (my $newhand = $hand) =~ s{J}{$subst}g;
+        my $type = gametype($newhand);
+        if ($tv{$best} < $tv{$type}) {
+            $best = $type;
+        }
+    }
+    return $best;
 }
