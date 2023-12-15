dab17a2e445fa6cd92e7d3d3f46cc469192de86d
Abe Timmerman <abeltje@cpan.org>
2023-12-15 14:51:25 +0100

    Add day 15; part 2

--- code/day15.pl
+++ code/day15.pl
@@ -27,6 +27,32 @@ if ($part == 1) {
     }
     say "Total: $total";
 }
+else {
+    my $total = 0;
+    open(my $in, '<', $input) or die "Cannot open($input): $!";
+    my $iseq = do { local $/; <$in> };
+    close($in);
+    $iseq =~ s{\n+}{,}g;
+    my @box = map { [] } 0 .. 255;
+    for my $item (split(/,/, $iseq)) {
+        my ($label) = $item =~ m{^ ([^=-]+) [=-] }x;
+        my $hash = calc_hash($label);
+        if ($item =~ m{^ \Q$label\E = ([0-9]+) $}x) {
+            replace_lens($box[$hash], $label, $1);
+        }
+        elsif ($item =~ m{^ \Q$label\E - $}x) {
+            remove_lens($box[$hash], $label);
+        }
+    }
+    for my $i (0 .. $#box) {
+        next unless @{$box[$i]};
+        for my $slot (0 .. $#{$box[$i]}) {
+            my $fp = ($i + 1) * ($slot + 1) * $box[$i]->[$slot][1];
+            $total += $fp;
+        }
+    }
+    say "Total: $total";
+}
 
 sub calc_hash ($string) {
     my $current = 0;
@@ -36,4 +62,18 @@ sub calc_hash ($string) {
         $current %= 256;
     }
     return $current;
+}
+
+sub replace_lens ($box, $label, $fl) {
+    for my $slot (@$box) {
+        if ($slot->[0] eq $label) {
+            $slot->[1] = $fl;
+            return;
+        }
+    }
+    push(@$box, [$label, $fl]);
+}
+
+sub remove_lens ($box, $label) {
+    @$box = grep { $_->[0] ne $label } @$box;
 }
