6956d9f0e6fe395e2c5d9bb8c36703df7353cadd
Abe Timmerman <abeltje@cpan.org>
2023-12-23 19:04:15 +0100

    Add day 5; part 2

--- code/day5.pl
+++ code/day5.pl
@@ -1,11 +1,12 @@
 #! /usr/bin/env perl -w
 use v5.38.1;
+no warnings 'experimental::for_list';
 use DDP;
 use File::Basename;
 use Getopt::Long qw< :config pass_through >;
 GetOptions(
-    'prd'    => \my $prd,
-    'part:1' => \my $part,
+    'prd'      => \my $prd,
+    'part|p:1' => \my $part,
 );
 $part //= 1;
 my ($day) = basename($0) =~ m/([0-9]+)/;
@@ -50,15 +51,94 @@ if ($part == 1) {
     close($in);
     say "Total: $total";
 }
+else {
+    my %almanac;
+    open(my $in, '<', $input) or die "Cannot open($input): $!";
+    local $/ = ""; # paragraph-mode
+    chomp(my $seeds = <$in>); $seeds =~ s{^seeds:\s+}{};
+
+    while (my $paragraph = <$in>) {
+        $paragraph =~ s{(\w+)-to-(\w+) map:\n}{};
+        my ($from, $to) = ($1, $2);
+        for my $mline (split(/\n/, $paragraph)) {
+            my ($dstart, $sstart, $range) = split(" ", $mline);
+            $almanac{ "${from}-${to}" }{ $sstart } = {
+                range => $range,
+                dest  => $dstart,
+            };
+        }
+    }
+    close($in);
+    my $min = $almanac{"humidity-location"}{ (sort keys %{$almanac{"humidity-location"}})[-1]}->{dest};
+    my @seeds = split(" ", $seeds);
+    my @locations;
+    for my ($start, $count) (@seeds) {
+        my $current = 'seed';
+        my ($section_name) = grep { $_ =~ m{^$current-} } keys %almanac;
+        say "Start lookup: $current ($section_name): $start - $count";
+        my @new_ranges = ([ $start, $count ]);
+        while ($section_name) {
+            print "$section_name: ";
+            my @temp_ranges;
+            for my $range (@new_ranges) {
+                push(@temp_ranges, lookup_range($almanac{$section_name}, @$range));
+            }
+            @new_ranges = map { [ @$_ ] } @temp_ranges;
+            @temp_ranges = ( );
+            ($current) = $section_name =~ m{^$current-(\w+)};
+            ($section_name) = grep { $_ =~ m{^$current-} } keys %almanac;
+say "@{[$section_name//'location']}: @$_" for @new_ranges;
+        }
+        push(@locations, map { $_->[0] } @new_ranges) if ! $section_name;
+    }
+    my $total = (sort {$a <=> $b} @locations)[0];
+    say "Total: $total";
+}
 
 
 sub lookup ($map, $value) {
     for my $src (sort keys %$map) {
         if ($value >= $src and $value < ($src + $map->{$src}{range})) {
             my $offset = abs($value - $src);
-say "$value ($src) => $map->{$src}{dest} + $offset";
             return $map->{$src}{dest} + $offset;
         }
     }
     return $value;
+}
+
+sub lookup_range ($section, $start, $count) {
+    # make the section contiguous
+    my @idx = sort {$a <=> $b} keys %$section;
+    if ($idx[0] > 0) {
+        $section->{0} = { range => $idx[0], dest => 0 };
+    }
+    my @list;
+    for my $begin (sort {$a <=> $b} keys %$section) {
+        push(@list, {min => $begin, max => $begin + $section->{$begin}{range} - 1});
+    }
+    # fill the gaps...!
+#say "Lookup range: $start .. @{[ $start + $count ]}";
+#say "$_->{min} .. $_->{max} ($section->{$_->{min}}{dest})" for @list;
+    my @ranges;
+    my $end = $start + $count - 1;
+    for my $cur (@list) {
+        if ($start >= $cur->{min} and $start <= $cur->{max}) {
+            my $offset = $start - $cur->{min};
+            if ($end <= $cur->{max}) {
+                push(@ranges, [ $section->{$cur->{min}}{dest} + $offset, $count ]);
+                last;
+            }
+            else {
+                my $overflow = $cur->{max} - $start;
+                $count -= $overflow;
+                push(@ranges, [ $section->{$cur->{min}}{dest} + $offset, $overflow ]);
+                $start = $cur->{max} + 1;
+            }
+        }
+    };
+    if ($start > $list[ $#list ]->{max}) {
+        push(@ranges, [$start, $count]);
+    }
+#say "Found: @$_" for @ranges;
+    return @ranges;
 }
