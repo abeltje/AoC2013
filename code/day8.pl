e89d553adaba5aa305d4644db67c7bce9c508eb2
Abe Timmerman <abeltje@cpan.org>
2023-12-23 14:35:58 +0100

    Add day8; part 2

--- code/day8.pl
+++ code/day8.pl
@@ -15,7 +15,6 @@ my $real = "data/day$day-$part";
 
 my $input = $prd ? $real : $sample;
 
-my (@cards, %cv, @type, %tv);
 if ($part == 1) {
     my %network;
     open(my $in, '<', $input) or die "Cannot open($input): $!";
@@ -36,5 +35,46 @@ say "$step ($idx): $node => $direction";
     say "Total: $step";
 }
 else {
-    say "Niet gedaan nog";
+    $|++;
+    my %network;
+    open(my $in, '<', $input) or die "Cannot open($input): $!";
+    chomp(my $instructions = do { local $/ = ""; <$in> });
+    while (my $line = <$in>) {
+        my ($n, $l, $r) = $line =~ m{^([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)};
+        $network{$n} = { L => $l, R => $r };
+    }
+    close($in);
+
+    my @nodes = grep { m{^ .+ A $}x } sort keys %network;
+    my @steps;
+    for my $node (@nodes) {
+        my $use_node = $node; # don't use alias
+        my $step = 0;
+        while ($use_node !~ m{^ .+ Z $}x) {
+            my $idx = $step % (length($instructions) - 1);
+            my $direction = substr($instructions, $idx, 1);
+            $use_node = $network{$use_node}->{$direction};
+            $step++;
+        }
+        push(@steps, $step);
+        say "$node => $use_node: $step";
+    }
+    my $kgv = shift(@steps);
+    while (@steps) {
+        $kgv = kgv($kgv, shift(@steps));
+    }
+    say "Total (kgv): $kgv";
+}
+
+sub kgv ($x, $y) {
+    return ($x * $y) / ggd($x, $y);
+}
+
+sub ggd ($x, $y) {
+    my $g = $x >  $y ? $x : $y;
+    my $k = $x <= $y ? $x : $y;
+    while ( my $r = $g % $k ) {
+        $g = $k; $k = $r;
+    }
+    return $k;
 }
